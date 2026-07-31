1ddaad92b407fe30a26382a6de8a712472ceffe5
Govind40674 <govindkumar4067444@outlook.com>
2026-07-31 19:16:03 +0530

    Remove YouTube controller, routes, and downloads folder

--- backend_node/routes/youtubeRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { downloadAudio } from "../controllers/youtubeController.js";
-
-const router = express.Router();
-
-router.post("/download", downloadAudio);
-
-export default router;
