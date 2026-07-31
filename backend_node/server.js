dbb0dc4b0d191882c04bdee991dd38f9224366e4
Govind40674 <govindkumar4067444@outlook.com>
2026-07-31 08:34:56 +0530

    changes in cors

--- backend_node/server.js
+++ backend_node/server.js
@@ -7,7 +7,7 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import videoRoutes from "./routes/videoRoutes.js";
 import historyRoutes from "./routes/historyRoutes.js";
-import profileRoutes from "./routes/profileRoutes.js"; // NEW
+import profileRoutes from "./routes/profileRoutes.js";
 
 dotenv.config();
 
@@ -15,14 +15,33 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.REACT_APP_URL,
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
+
+app.options("*", cors());
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-
 app.use("/", videoRoutes);
 app.use("/", historyRoutes);
-app.use("/", profileRoutes); // NEW
+app.use("/", profileRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend Running");
